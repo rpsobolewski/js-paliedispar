d3775b69a5033f8079958cc7ce0b73afb6b52abf
rpsobolewski <rpsobolewski@gmail.com>
2023-08-31 14:00:39 +0200

    flipped the word in a new const

--- palindromo/assets/js/main.js
+++ palindromo/assets/js/main.js
@@ -1,6 +1,11 @@
 /* Chiedere all’utente di inserire una parola Creare una funzione per capire se la parola inserita è palindroma */
 
 //ciedere all'utente di inserire una parola
+const parolaUtente = prompt("inserire una parola");
+console.log(parolaUtente);
 //assegnare la stessa parola ma scritta al contrario 
+const parolaContrario = parolaUtente.split('').reverse().join('');
+console.log(parolaContrario)
+
 //confrontare con un ciclo while se la parola è uguale
 //stampare il risultato
