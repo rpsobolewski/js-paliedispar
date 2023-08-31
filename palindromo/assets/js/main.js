5f4c8d3ef6416be24251094ae4855a05e6554595
rpsobolewski <rpsobolewski@gmail.com>
2023-08-31 14:09:46 +0200

    logic working

--- palindromo/assets/js/main.js
+++ palindromo/assets/js/main.js
@@ -8,4 +8,27 @@ const parolaContrario = parolaUtente.split('').reverse().join('');
 console.log(parolaContrario)
 
 //confrontare con un ciclo while se la parola è uguale
+
+let palindromo = false;
+let i = 0;
+const lunghezza = parolaUtente.length;
+while (i < lunghezza){
+    if (parolaUtente[i] != parolaContrario[i]){
+
+        break
+    } else {
+        i++;
+        
+        palindromo = true;
+    }
+
+}
+
+
+
 //stampare il risultato
+if (palindromo){
+    console.log("la parola è un palindromo");
+} else {
+    console.log("la parola non è un palindromo");
+}
