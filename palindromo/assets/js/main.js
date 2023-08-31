6515ea0cdcbd3e77847c36ea10227459a9c6f38a
rpsobolewski <rpsobolewski@gmail.com>
2023-08-31 13:47:04 +0200

    logica

---
+++ palindromo/assets/js/main.js
@@ -0,0 +1,6 @@
+/* Chiedere all’utente di inserire una parola Creare una funzione per capire se la parola inserita è palindroma */
+
+//ciedere all'utente di inserire una parola
+//assegnare la stessa parola ma scritta al contrario 
+//confrontare con un ciclo while se la parola è uguale
+//stampare il risultato
